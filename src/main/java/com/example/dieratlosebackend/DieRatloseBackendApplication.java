5b2213b2fa9b756665b4d03db4910139f79fc382
ThildaEles <thilda.eles@gmail.com>
2023-04-12 22:04:46 +0200

    Initial commit

---
+++ src/main/java/com/example/dieratlosebackend/DieRatloseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dieratlosebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieRatloseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DieRatloseBackendApplication.class, args);
+    }
+
+}
